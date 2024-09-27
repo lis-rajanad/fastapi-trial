8ed770ca94bdad33b7b50bc53c17240fa28e1283
Rajan Adhikari <rajan.adhikari@yco.com.np>
2024-09-27 16:52:29 +0545

    add endpoint for item

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -11,3 +12,16 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
